e6d7271e00925badbdf50215d67f6c016f5758ac
Desarrollo <desarrollo_cchc@hotmail.com>
2021-02-24 17:06:28 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Hero from './components/about/hero';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from  './components/testimonials/testimonials';
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <About />
+      <Resume />
+      <Portfolio />
+      <Testimonials />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
